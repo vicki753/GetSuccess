2ef2b0946e8cb7faa9e9725925e52dd72a88f502
vicki753 <zhengyi753@qq.com>
2018-10-15 16:24:47 +0800

    feat():增加collectionview和rectNative

---
+++ ZYCollectionViewDemo/ZYCollectionViewDemo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  ZYCollectionViewDemo
+//
+//  Created by 郑奕 on 2018/10/8.
+//  Copyright © 2018 郑奕. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ ZYRectNativeDemo/ZYRectNativeDemo/ViewController.m
@@ -0,0 +1,35 @@
+//
+//  ViewController.m
+//  ZYRectNativeDemo
+//
+//  Created by 郑奕 on 2018/10/15.
+//  Copyright © 2018 郑奕. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+    
+    @try {
+        
+    }@finally {
+        
+    }
+}
+
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+@end
